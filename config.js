7aa5e000fc747e059206d0e5d2023f60a8acf350
salahW3b <113988161+salahW3b@users.noreply.github.com>
2022-12-13 18:07:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "MTA0MDkxNzE2NTIzMjA5OTM0MQ.GQQRTL.ilun2MJ0-Dq6tXift1to-iKkQISnbz_sEH4ISQ", 
-    guildid: "9572510799118",
-    url: "wallahleself" 
+    token: "", //Ton TOken 
+    guildid: "", // Id serv
+    url: "" // Url a snipe
 }
